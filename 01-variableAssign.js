32bbcf4a58101aa1ffc928f6bd53f20dc84a6ca3
Hemant Kendre <33821024+hemantkendre@users.noreply.github.com>
2025-05-06 16:26:42 +0530

    Created 01-variableAssign.js file
    Created 01-variableAssign.js file

---
+++ 01-variableAssign.js
@@ -0,0 +1,34 @@
+console.log("=================== task 1 =====================");
+
+var myFullName;
+console.log("myFullName before initialization is :",myFullName);
+myFullName = "Hemant kendre";
+console.log("myFullName after initialization is :",myFullName);
+myFullName = "Hemant Dushyant kendre";
+console.log("myFullName after updation is : ",myFullName);
+
+console.log("=================== task 2 =====================");
+var myLove = "I love only JS";
+console.log(myLove);
+
+console.log("=================== task 3 =====================");
+var firstName = "Hemant";
+console.log("First Name :",firstName);
+var lastName = "Kendre";
+console.log("Last Name :",lastName);
+var age = 36;
+console.log("Age :",age);
+var address = "Wakad, Pune";
+console.log("Address :",address);
+var pinCode = 411039
+console.log("Pin Code :",pinCode);
+var gender = "Male";
+console.log("Gender :",gender);
+var isMarried = true;
+console.log("Is Married :",isMarried);
+
+
+
+
+
+
