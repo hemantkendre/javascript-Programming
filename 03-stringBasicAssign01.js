ed95affc2852941a60893510d41d9dac72a228df
Hemant Kendre <33821024+hemantkendre@users.noreply.github.com>
2025-05-09 15:04:36 +0530

    03-stringBasicAssign01.js
    Created 03-stringBasicAssign01.js file

---
+++ 03-stringBasicAssign01.js
@@ -0,0 +1,12 @@
+function stringBasics(){
+    var dreamCompany = "Apple";
+    console.log(`My dream company is ${dreamCompany}`);
+    
+    var hobbyOne = "Swimming";
+    var hobbyTwo = "Cricket";
+    var hobbyThree = "Reading";
+
+    console.log(`My hobbies are ${hobbyOne}, ${hobbyTwo}, ${hobbyThree}`);
+    console.log(`Sum of total number of characters are : ${hobbyOne.length + hobbyTwo.length + hobbyThree.length} `);
+}
+stringBasics();
